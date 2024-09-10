490ef81c07e7b904a0d5f4933221f17e45848019
royhanmh <royhanm35@gmail.com>
2024-09-10 10:14:55 +0700

    first commit

---
+++ app/Http/Controllers/admin/AdminController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminController extends Controller
+{
+    public function AdminLogout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    }
+}
