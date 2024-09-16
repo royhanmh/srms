ede8f0801586e4fa5850552426bfefd270224c09
royhanmh <royhanm35@gmail.com>
2024-09-16 17:10:29 +0700

    establish relationship from student to classes db

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Student extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    public function class(): BelongsTo
+    {
+        return $this->belongsTo(classes::class, 'class_id', 'id');
+    }
 }
